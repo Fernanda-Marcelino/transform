d8d4e7706a6fcc8241ab458d7c42c9db6fbffe7e
Fernanda-Marcelino <fernanda.marcelino2@etec.sp.gov.br>
2023-10-21 20:38:48 -0300

    teste comandos

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 const canvas = document.getElementById('puzzle');
 const ctx = canvas.getContext('2d');
 
+ctx.fillStyle = "#362656";
+ctx.fillRect(0,700,90,500);
+ctx.translate(90, 100);
+ctx.fillRect(0,700,90,800);
 
